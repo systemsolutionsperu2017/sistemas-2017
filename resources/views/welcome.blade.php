c8d6a8c13c7870f01be8248b88ca1e8a47f39592
systemsolutionsperu2017 <34246291+systemsolutionsperu2017@users.noreply.github.com>
2017-12-07 12:34:19 -0500

    Editando Titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    SISTEMAS-2017
                 </div>
 
                 <div class="links">
